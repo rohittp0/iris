c1803c13382eb02b69685825458fb4b827129fa9
Rohit T P <tprohit9@gmail.com>
2022-08-31 22:35:54 +0530

    new data

---
+++ dispersion.py
@@ -0,0 +1,20 @@
+import cv2
+from sklearn.preprocessing import normalize
+from scipy.special import entr
+import matplotlib.pyplot as plt
+
+polar = cv2.imread("data/output/02/1R/S3021R03.jpg/polar.png", cv2.IMREAD_GRAYSCALE)
+
+polar_normal_row = normalize(polar)
+polar_normal_col = normalize(polar, axis=0)
+
+dispersion_rows = entr(polar_normal_row).sum(axis=1)
+dispersion_cols = entr(polar_normal_col).sum(axis=0)
+
+for title, data in [("Row", dispersion_rows), ("Column", dispersion_cols)]:
+    figure, axis = plt.subplots()
+    axis.set_title("Dispersion along "+title)
+    axis.set_xlabel(title)
+    axis.plot(data)
+
+    figure.savefig("data/" + title)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import glob
 
+import cv2
+import numpy as np
+
+from utils import show_image
+
 
 def main():
     files = glob.glob(r"data\IITD Database\**\*.*", recursive=True)
@@ -12,5 +17,17 @@ def main():
             readme.write(out)
 
 
-if __name__ == "__main__":
-    main()
+i = cv2.imread("data/output/02/1R/S3021R03.jpg/polar.png", 0)
+
+i[:, 20:100] = 0
+i[:, 140:220] = 0
+
+j = np.zeros((20, 60))
+
+j[:, :20] = i[:, :20]
+j[:, 20:40] = i[:, 100:120]
+j[:, 40:60] = i[:, 220:]
+
+cv2.imwrite("roi.png", j)
+# if __name__ == "__main__":
+#     main()
