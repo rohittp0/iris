6f666a5392b812d56d4a2083cf099278ed18aa2d
Rohit T P <tprohit9@gmail.com>
2022-10-20 20:30:05 +0530

    new code

--- fnc/segment.py
+++ fnc/segment.py
@@ -1,177 +1,76 @@
-##-----------------------------------------------------------------------------
-##  Import
-##-----------------------------------------------------------------------------
+import cv2
 import numpy as np
-from fnc.boundary import searchInnerBound, searchOuterBound
-from fnc.line import findline, linecoords
-import multiprocessing as mp
+
+from utils import show_image
+
+kernel = np.ones((5, 5), np.uint8)
 
 
-##-----------------------------------------------------------------------------
-##  Function
-##-----------------------------------------------------------------------------
-def segment(eyeim, minrad, multiplier, sigma, virt, horiz, eyelashes_thres=80, use_multiprocess=True):
-    """
-    Description:
-        Segment the iris region from the eye image.
-        Indicate the noise region.
+def transform_image(img, threshold):
+    retval, threshold = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
 
-    Input:
-        eyeim				- Eye image
-        eyelashes_thres   	- Eyelashes threshold
-        use_multiprocess   	- Use multiprocess to run
+    opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel)
 
-    Output:
-        ciriris		- Centre coordinates and radius of iris boundary.
-        cirpupil	- Centre coordinates and radius of pupil boundary.
-        imwithnoise	- Original image with location of noise marked with NaN.
-    """
-    # Find the iris boundary by Daugman's intefro-differential
-    rowp, colp, rp = searchInnerBound(eyeim, minrad, multiplier)
-    row, col, r = searchOuterBound(eyeim, rowp, colp, rp)
+    open_close = cv2.bitwise_or(opening, closing, mask=None)
 
-    # Package pupil and iris boundaries
-    rowp = np.round(rowp).astype(int)
-    colp = np.round(colp).astype(int)
-    rp = np.round(rp).astype(int)
-    row = np.round(row).astype(int)
-    col = np.round(col).astype(int)
-    r = np.round(r).astype(int)
-    cirpupil = [rowp, colp, rp]
-    ciriris = [row, col, r]
+    return open_close
 
-    # Find top and bottom eyelid
-    imsz = eyeim.shape
-    irl = np.round(row - r).astype(int)
-    iru = np.round(row + r).astype(int)
-    icl = np.round(col - r).astype(int)
-    icu = np.round(col + r).astype(int)
-    if irl < 0:
-        irl = 0
-    if icl < 0:
-        icl = 0
-    if iru >= imsz[0]:
-        iru = imsz[0] - 1
-    if icu >= imsz[1]:
-        icu = imsz[1] - 1
-    imageiris = eyeim[irl: iru + 1, icl: icu + 1]
 
-    # If use_multiprocess
-    if use_multiprocess:
-        ret_top = mp.Manager().dict()
-        ret_bot = mp.Manager().dict()
-        p_top = mp.Process(
-            target=findTopEyelid,
-            args=(imsz, imageiris, irl, icl, rowp, rp, sigma, virt, horiz, ret_top),
-        )
-        p_bot = mp.Process(
-            target=findBottomEyelid,
-            args=(imsz, imageiris, rowp, rp, irl, icl, sigma, virt, horiz, ret_bot),
-        )
-        p_top.start()
-        p_bot.start()
-        p_top.join()
-        p_bot.join()
-        mask_top = ret_top[0]
-        mask_bot = ret_bot[0]
+def segment(i):
+    golden_reference = sum(sum(transform_image(i, 0)))
+    working_img = None
+    c = i.copy()
 
-    # If not use_multiprocess
+    for k in range(10, 1000, 10):
+
+        working_img = transform_image(i, k)
+        summing = sum(sum(working_img))
+        difference = summing - golden_reference
+
+        if difference > 800:
+            break
+
+    _, contours = cv2.findContours(working_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    if contours is None:
+        return
+
+    for z in contours:
+
+        x, y, w, h = cv2.boundingRect(z)
+        if x + w < 150 and y + h < 200 and x - w // 4 > 0:
+            cv2.rectangle(working_img, (x, y), (x + w, y + h), (0, 255, 0), -2)
+
+    _, contours_2 = cv2.findContours(working_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    maximum_area = 0
+    maximum_width = 0
+    point_x = 0
+    point_y = 0
+    maximum_height = 0
+
+    for z in contours_2:
+        x, y, w, h = cv2.boundingRect(z)
+        new_area = h * w
+        if x + w < 150 and y + h < 200 and new_area > maximum_area and x - w // 4 > 0:
+            maximum_area = new_area
+            maximum_width = w
+            point_x = x
+            point_y = y
+            maximum_height = h
+
+    center_x = point_x + maximum_width // 2
+    center_y = point_y + maximum_height // 2
+    radius = 40
+
+    if center_y - radius > 0 and center_x - radius > 0 and center_y + radius < 200 and center_x + radius < 150:
+        new_roi = c[center_y - radius:center_y + radius, center_x - radius:center_x + radius]
+        new_roi = cv2.resize(new_roi, (200, 150))
     else:
-        mask_top = findTopEyelid(imsz, imageiris, irl, icl, rowp, sigma, virt, horiz, rp)
-        mask_bot = findBottomEyelid(imsz, imageiris, rowp, rp, irl, icl, sigma, virt, horiz)
+        center_y = c.shape[0] // 2
+        center_x = c.shape[1] // 2
+        new_roi = c[center_y - radius:center_y + radius, center_x - radius:center_x + radius]
+        new_roi = cv2.resize(new_roi, (200, 150))
 
-    # Mask the eye image, noise region is masked by NaN value
-    imwithnoise = eyeim.astype(float)
-    imwithnoise = imwithnoise + mask_top + mask_bot
-
-    # For CASIA, eliminate eyelashes by threshold
-    ref = eyeim < eyelashes_thres
-    coords = np.where(ref == 1)
-    imwithnoise[coords] = np.nan
-
-    return ciriris, cirpupil, imwithnoise
-
-
-# ------------------------------------------------------------------------------
-def findTopEyelid(imsz, imageiris, irl, icl, rowp, rp, sigma, virt, horiz, ret_top=None):
-    """
-    Description:
-        Mask for the top eyelid region.
-
-    Input:
-        imsz		- Size of the eye image.
-        imageiris	- Image of the iris region.
-
-        irl		    -
-        icl		    -
-
-        rowp		- y-coordinate of the inner circle centre.
-        rp		    - radius of the inner circle centre.
-
-        ret_top		- Just used for returning result when using multiprocess.
-
-    Output:
-        mask    	- Map of noise that will be masked with NaN values.
-    """
-    topeyelid = imageiris[0: rowp - irl - rp, :]
-    lines = findline(topeyelid, sigma, virt, horiz)
-    mask = np.zeros(imsz, dtype=float)
-
-    if lines.size > 0:
-        xl, yl = linecoords(lines, topeyelid.shape)
-        yl = np.round(yl + irl - 1).astype(int)
-        xl = np.round(xl + icl - 1).astype(int)
-
-        yla = np.max(yl)
-        y2 = np.arange(yla)
-
-        mask[yl, xl] = np.nan
-        grid = np.meshgrid(y2, xl)
-        mask[tuple(grid)] = np.nan
-
-    # Return
-    if ret_top is not None:
-        ret_top[0] = mask
-    return mask
-
-
-# ------------------------------------------------------------------------------
-def findBottomEyelid(imsz, imageiris, rowp, rp, irl, icl, sigma, virt, horiz, ret_bot=None):
-    """
-    Description:
-        Mask for the bottom eyelid region.
-
-    Input:
-        imsz		- Eye image.
-        imageiris	- Image of the iris region.
-
-        rowp		- y-coordinate of the inner circle centre.
-        rp		    - radius of the inner circle centre.
-
-        irl		    -
-        icl		    -
-
-        ret_bot		- Just used for returning result when using multiprocess.
-
-    Output:
-        mask    	- Map of noise that will be masked with NaN values.
-    """
-    bottomeyelid = imageiris[rowp - irl + rp - 1: imageiris.shape[0], :]
-    lines = findline(bottomeyelid, sigma, virt, horiz)
-    mask = np.zeros(imsz, dtype=float)
-
-    if lines.size > 0:
-        xl, yl = linecoords(lines, bottomeyelid.shape)
-        yl = np.round(yl + rowp + rp - 3).astype(int)
-        xl = np.round(xl + icl - 2).astype(int)
-        yla = np.min(yl)
-        y2 = np.arange(yla - 1, imsz[0])
-
-        mask[yl, xl] = np.nan
-        grid = np.meshgrid(y2, xl)
-        mask[tuple(grid)] = np.nan
-
-    # Return
-    if ret_bot is not None:
-        ret_bot[0] = mask
-    return mask
+    show_image(new_roi, i)
--- main.py
+++ main.py
@@ -9,42 +9,31 @@ from fnc.segment import segment
 from utils import show_image
 
 
-def transform_image(img, threshold):
-    retval, threshold = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-
-    opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel)
-
-    open_close = cv2.bitwise_or(opening, closing, mask=None)
-
-    return open_close
-
-
 def process_image(im_path, out_path):
     im = cv2.imread(im_path, 0)
 
-    ciriris, cirpupil, imwithnoise = segment(im, **params, eyelashes_thres=80)
+    segment(im)
 
-    cv2.circle(im, [*reversed(ciriris[:2])], ciriris[2], (0, 255, 0), 2)
-    cv2.circle(im, [*reversed(cirpupil[:2])], cirpupil[2], (255, 0, 255), 2)
-
-    pathlib.Path(out_path).parent.mkdir(parents=True, exist_ok=True)
-
-    polar_array, noise_array = normalize(imwithnoise, ciriris[1], ciriris[0], ciriris[2],
-                                         cirpupil[1], cirpupil[0], cirpupil[2], radial_res=20, angular_res=240)
-
-    polar_array = cv2.cvtColor(np.asarray(polar_array * 255, dtype=np.uint8), cv2.COLOR_GRAY2BGR)
-    noise_array = cv2.cvtColor(np.asarray(noise_array * 255, dtype=np.uint8), cv2.COLOR_GRAY2BGR)
-
-    out_path = out_path.split(".")[0]
-
-    cv2.imwrite(f"{out_path}_polar.png", polar_array)
-    cv2.imwrite(f"{out_path}_noise.png", noise_array)
-    cv2.imwrite(f"{out_path}_eye.png", im)
-
-    np.savetxt(f"{out_path}_polar.txt", cv2.cvtColor(polar_array, cv2.COLOR_BGR2GRAY))
-    np.savetxt(f"{out_path}_noise.txt", cv2.cvtColor(noise_array, cv2.COLOR_BGR2GRAY))
-    np.savetxt(f"{out_path}_eye.txt", im)
+    # cv2.circle(im, [*reversed(ciriris[:2])], ciriris[2], (0, 255, 0), 2)
+    # cv2.circle(im, [*reversed(cirpupil[:2])], cirpupil[2], (255, 0, 255), 2)
+    #
+    # pathlib.Path(out_path).parent.mkdir(parents=True, exist_ok=True)
+    #
+    # polar_array, noise_array = normalize(imwithnoise, ciriris[1], ciriris[0], ciriris[2],
+    #                                      cirpupil[1], cirpupil[0], cirpupil[2], radial_res=20, angular_res=240)
+    #
+    # polar_array = cv2.cvtColor(np.asarray(polar_array * 255, dtype=np.uint8), cv2.COLOR_GRAY2BGR)
+    # noise_array = cv2.cvtColor(np.asarray(noise_array * 255, dtype=np.uint8), cv2.COLOR_GRAY2BGR)
+    #
+    # out_path = out_path.split(".")[0]
+    #
+    # cv2.imwrite(f"{out_path}_polar.png", polar_array)
+    # cv2.imwrite(f"{out_path}_noise.png", noise_array)
+    # cv2.imwrite(f"{out_path}_eye.png", im)
+    #
+    # np.savetxt(f"{out_path}_polar.txt", cv2.cvtColor(polar_array, cv2.COLOR_BGR2GRAY))
+    # np.savetxt(f"{out_path}_noise.txt", cv2.cvtColor(noise_array, cv2.COLOR_BGR2GRAY))
+    # np.savetxt(f"{out_path}_eye.txt", im)
 
 
 def main():
